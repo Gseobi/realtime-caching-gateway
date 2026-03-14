ff76bbe2ad392f846d0e780c930dee2c9347b39f
GSEOBI <wsx2386@naver.com>
2026-03-14 12:31:44 +0900

    Initial commit

---
+++ src/test/java/java/com/github/gseobi/caching/gateway/cachinggateway/RealtimeCachingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.github.gseobi.caching.gateway.cachinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeCachingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
